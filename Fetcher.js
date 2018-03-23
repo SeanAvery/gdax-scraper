91916458ce1af12d12ce5e4ce1ae5f3a6b50863e
Sean Pollock <seanpollock3344@gmail.com>
2018-03-22 22:03:31 -0400

    ws connection to gdax stream

--- Fetcher.js
+++ Fetcher.js
@@ -1,19 +1,23 @@
-export default class Fetcher {
-  constructor(params) {
+// export default class Fetcher {
+//  constructor(params) {
         
-  }
-}
+//  }
+//}
 
+import WS from 'ws'
 export class Websocket {
   constructor(params) {
-    
+    this.ws = new WS(params.wsEndpoint)
+    console.log('this.ws', this.ws)   
   }
 }
 
 const tests = async () => {
-  const params = {
+  const wsParams = {
     'wsEndpoint': 'wss://ws-feed-public.sandbox.gdax.com'
   }
-
+  const websocket = new Websocket(wsParams)
 }
+
+tests()
 
