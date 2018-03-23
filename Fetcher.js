0ed38ffadd41712a9098e1b979189224893fd4e7
Sean Pollock <seanpollock3344@gmail.com>
2018-03-22 21:29:43 -0400

    init commit

---
+++ Fetcher.js
@@ -0,0 +1,19 @@
+export default class Fetcher {
+  constructor(params) {
+        
+  }
+}
+
+export class Websocket {
+  constructor(params) {
+    
+  }
+}
+
+const tests = async () => {
+  const params = {
+    'wsEndpoint': 'wss://ws-feed-public.sandbox.gdax.com'
+  }
+
+}
+
